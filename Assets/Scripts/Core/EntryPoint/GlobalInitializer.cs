5656814efa79b5975610d0b9ec5f51447b198c3a
Vitalii Volkov <volkov181295@gmail.com>
2025-04-09 19:27:26 +0300

    Cleanup

--- Assets/Scripts/Core/EntryPoint/GlobalInitializer.cs
+++ Assets/Scripts/Core/EntryPoint/GlobalInitializer.cs
@@ -1,19 +1,18 @@
 using Core.Loaders.Scene;
-using Cysharp.Threading.Tasks;
 using Tools.Constants;
 using UnityEngine;
 using Zenject;
 
 namespace Core.EntryPoint {
   public class GlobalInitializer : MonoBehaviour {
-    
+
     private void Start() {
       EnterInGame();
     }
 
     private async void EnterInGame() {
       var sceneLoader = ProjectContext.Instance.Container.Resolve<ISceneLoader>();
-       await sceneLoader.LoadSceneAsync(SceneNameConstants.LoadingSceneKey);
+      await sceneLoader.LoadSceneAsync(SceneNameConstants.LoadingSceneKey);
     }
   }
 }
